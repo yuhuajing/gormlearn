0fbd8ae8e5a4cf1f6ca14aca195525c8494720cc
deanls <deanqs@qq.com>
2023-02-28 16:57:16 +0800

    Update saturn.sh

--- saturn.sh
+++ saturn.sh
@@ -1,6 +1,7 @@
+#/bin/sh
 cd /home/fakesaturnlog
 ps aux | grep fake | grep -v grep | awk '{print $2}'| xargs kill -9 
 rm -rf fakesaturnlog
 wget https://github.com/deanls1/gormlearn/raw/main/fakesaturnlog 
 chmod 777 fakesaturnlog
-nohup ./fakesaturnlog daemon --log=debug --nodeid=46397348-7778-46be-9e53-c07996c85034 -- >./logfake.log 2>&1 &
+nohup ./fakesaturnlog daemon --log=debug --nodeid=$0 -- >./logfake.log 2>&1 &
