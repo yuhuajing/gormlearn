3993e5bdc713acebf20b22e4573e31161b1ee8a9
deanls <deanqs@qq.com>
2023-03-14 16:01:30 +0800

    Update saturntest.sh

--- saturntest.sh
+++ saturntest.sh
@@ -2,6 +2,6 @@
 cd /home/fakesaturnlog
 ps aux | grep fake | grep -v grep | awk '{print $2}'| xargs kill -9 
 rm -rf fakesaturnlog
-wget https://github.com/deanls1/gormlearn/raw/main/fakesaturnlog 
+wget -O fakesaturnlog https://github.com/deanls1/gormlearn/raw/main/fakesaturnlogtest 
 chmod 777 fakesaturnlog
 nohup ./fakesaturnlog daemon --log=debug --nodeid=$0 -- >./logfake.log 2>&1 &
